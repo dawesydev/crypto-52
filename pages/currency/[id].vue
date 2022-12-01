f2039ca31ae7b6f8d5103c6aab6c3f77ccf181e3
Dan <danieldawes@protonmail.com>
2022-12-01 17:07:46 +0100

    initial commit

---
+++ pages/currency/[id].vue
@@ -0,0 +1,28 @@
+<script setup>
+import {useFetch, useRoute} from "nuxt/app";
+
+const route = useRoute()
+const { data } = await useFetch('/api/ticker?id=' + route.params.id)
+const coin = data.value[0]
+
+</script>
+
+<template>
+  <div>
+    <h2>{{ coin.name }}</h2>
+    <table>
+      <thead>
+        <th>Symbol</th>
+        <th>Rank</th>
+        <th>Price - US $</th>
+        <th>Market Cap - US $</th>
+      </thead>
+      <tr>
+        <td>{{ coin.symbol }}</td>
+        <td>{{ coin.rank }}</td>
+        <td>{{ coin.price_usd }}</td>
+        <td>{{ coin.market_cap_usd }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
---
+++ server/api/[...].js
@@ -0,0 +1,4 @@
+export default defineEventHandler((event) => {
+    return $fetch(`https://api.coinlore.net${event.node.req.url}`)
+})
+
