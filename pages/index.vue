a3d70c4d9c94eb47e42ea67b206aa488b138e683
Dan <danieldawes@protonmail.com>
2023-01-28 08:32:05 +0000

    layout fixed

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ const { data } = await useFetch('/api/global')
 </script>
 
 <template>
-  <main class="h-screen text-gray-100">
+  <main class="min-h-fit text-gray-100">
     <div class="flex flex-col items-center place-content-between">
       <div class="flex flex-col items-center">
         <h1 class="font-display text-3xl tracking-wide">Global Data Dashboard</h1>
